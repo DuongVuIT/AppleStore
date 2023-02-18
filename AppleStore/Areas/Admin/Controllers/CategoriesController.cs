65cf4e4397cdcad44815caae8e23a44fc20261ee
C:/Program Files/Git/DUONGVU <duonghutech26@gmail.com>
2023-02-18 18:14:53 +0700

    chỉnh sửa lại chuỗi kết nối.

---
+++ AppleStore/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,34 @@
+﻿
+using AppleStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppleStore.Areas.Admin.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        AppleStoreEntities db = new AppleStoreEntities();
+        // GET: Admin/Categories
+        public ActionResult Index()
+        {
+            var lsp = db.Loai_San_Pham.ToList();
+
+            return View(lsp);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create( Loai_San_Pham lsp)
+        {
+            db.Loai_San_Pham.Add(lsp);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+    }
+}
