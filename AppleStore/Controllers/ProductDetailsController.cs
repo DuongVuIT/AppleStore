c52bce66763e8177550ce5c322b4299d1d616a37
C:/Program Files/Git/DUONGVU <duonghutech26@gmail.com>
2023-02-18 14:18:42 +0700

    first commit

---
+++ AppleStore/Controllers/ProductDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppleStore.Controllers
+{
+    public class ProductDetailsController : Controller
+    {
+        // GET: ProduceDetails
+        public ActionResult DetailsPro()
+        {
+            return View();
+        }
+    }
+}
