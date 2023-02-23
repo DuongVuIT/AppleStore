a9748df88c3be5063d4f33d653056edc26662fc4
C:/Program Files/Git/DUONGVU <duonghutech26@gmail.com>
2023-02-23 13:32:47 +0700

    chinh sua delete,  id loai sp

--- AppleStore/Areas/Admin/Controllers/ProductsController.cs
+++ AppleStore/Areas/Admin/Controllers/ProductsController.cs
@@ -1,8 +1,10 @@
 ﻿using AppleStore.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Web;
 using System.Web.Mvc;
 
@@ -20,7 +22,7 @@ namespace AppleStore.Areas.Admin.Controllers
         [HttpGet]
         public ActionResult Create()
         {
-            ViewBag.idloaiSP = new SelectList(db.Loai_San_Pham, "id", "tenloaiSP");
+            ViewBag.idloaiSP = new SelectList(db.Loai_San_Pham, "id", "tenloaiSP"); 
             return View();
         }
         [HttpPost]
@@ -45,8 +47,45 @@ namespace AppleStore.Areas.Admin.Controllers
             var sp = db.San_Pham.Where(n => n.id == id).FirstOrDefault();
             return View(sp);
         }
-        
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
 
+            var sp = db.San_Pham.Where(n => n.id == id).FirstOrDefault();
+            return View(sp);
+        }
+        [HttpPost]
+
+        public ActionResult Delete(San_Pham ssp)
+        {
+            var sp = db.San_Pham.Where(n => n.id == ssp.id).FirstOrDefault();
+            db.San_Pham.Remove(sp);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var sp = db.San_Pham.Where(n=>n.id==id).FirstOrDefault();
+            ViewBag.idloaiSP = new SelectList(db.Loai_San_Pham, "id", "tenloaiSP", sp.idloaiSP);
+            return View(sp);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, San_Pham sp, HttpPostedFileBase ImageUpload)
+        {
+            if (ImageUpload != null && ImageUpload.ContentLength > 0)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(ImageUpload.FileName);
+                string extension = Path.GetExtension(ImageUpload.FileName);
+                fileName = fileName + extension;
+                sp.hinh = fileName;
+                ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/Images"), fileName));
+            }
+
+            db.Entry(sp).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
     }
 }
