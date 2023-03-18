974d3d6f366599097b0a862b2f85d89e84db13e0
C:/Program Files/Git/DUONGVU <duonghutech26@gmail.com>
2023-03-18 16:52:09 +0700

    chỉnh sửa background, color button

--- AppleStore/Models/AppleStore.Designer.cs
+++ AppleStore/Models/AppleStore.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\Workspace\Đồ án asp.net\AppleStore\AppleStore\AppleStore\Models\AppleStore.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ACER\Source\Repos\AppleStore\AppleStore\Models\AppleStore.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
